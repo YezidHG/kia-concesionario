7659ef20604dace49f8019ee26675eab7ee33915
YezidHG <yezidesteban5@gmail.com>
2025-04-01 20:26:48 -0500

    Se creo el proyecto de la app

---
+++ app/kia-concesionario/kia-concesionario/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace kia_concesionario.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
