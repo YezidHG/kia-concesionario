feda1454bd83a637af37641ffe827e65279460c4
YezidHG <yezidesteban5@gmail.com>
2025-04-08 21:42:08 -0500

    se arreglaron problemas del frontend

--- app/kia-concesionario/kia-concesionario/Models/Cita.cs
+++ app/kia-concesionario/kia-concesionario/Models/Cita.cs
@@ -1,15 +1,25 @@
 ﻿namespace kia_concesionario.Models
 {
     using System;
+    using System.ComponentModel.DataAnnotations.Schema;
 
     namespace kia_concesionario.Models
     {
         public class Cita
         {
+            [Column("ID_CITA")]
             public int Id_cita { get; set; }
+
+            [Column("CEDULA_CLIENTE")]
             public string Cedula_cliente { get; set; }
+
+            [Column("FECHA_CITA")]
             public DateTime Fecha_cita { get; set; }
+
+            [Column("HORA_CITA")]
             public string Hora_cita { get; set; }
+
+            [Column("ESTADO")]
             public string Estado { get; set; }
         }
     }
