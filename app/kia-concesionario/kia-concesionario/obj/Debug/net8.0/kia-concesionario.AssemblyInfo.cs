621aca1425d26e3c50bc02fc9193d210fdfe6ce8
FabianDorado <142599451+FabianDorado@users.noreply.github.com>
2025-05-03 13:45:54 -0500

    arreglando errores de los modelos 2

--- app/kia-concesionario/kia-concesionario/obj/Debug/net8.0/kia-concesionario.AssemblyInfo.cs
+++ app/kia-concesionario/kia-concesionario/obj/Debug/net8.0/kia-concesionario.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("kia-concesionario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+376fa13112b35b2450f930d9285b20d4eece5621")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a2ad90cf84d58ac411e9fcbb0be8b52882d3dbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("kia-concesionario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("kia-concesionario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
